9f5a03dbf45458e290be3036eae44016a60faa23
keremoner <kerem.onr@gmail.com>
2023-07-23 11:21:22 +0200

    Fl analysis script

--- scripts/Analysis.py
+++ scripts/Analysis.py
@@ -19,6 +19,7 @@ import ast
 import numpy as np
 import argparse
 from sklearn.preprocessing import OneHotEncoder
+import pickle
 
 def encode_maze(maze):
     num_rows = len(maze)
@@ -64,7 +65,7 @@ def file_dir(relative_path):
     return os.path.join(absolute_path, relative_path)
     
 if __name__ == "__main__":
-    directory = file_dir("../datasets/after_bug/states_s1-100_t500_Cartpole-v1/")
+    directory = file_dir("../datasets/after_bug/FrozenLake-v1_m4-4_s1-100_t1/")
     #directory = "../datasets/10k/"
     dataset_names = os.listdir(directory)
     dataset = pd.DataFrame()
@@ -86,9 +87,9 @@ if __name__ == "__main__":
         dataset['Encoded_Map'] = dataset['Map'].apply(lambda x: encode_maze(x))
         dataset['OneHotEncoded_Map'] = dataset['Map'].apply(lambda x: np.reshape(encode_map(x)[0], (-1)))
     
-    features = ['Simulations', 'Cart Position', 'Cart Velocity', 'Pole Angle', 'Pole Angular Velocity']
+    #features = ['Simulations', 'Cart Position', 'Cart Velocity', 'Pole Angle', 'Pole Angular Velocity']
     #features = ['Simulations']
-    #features = ['Simulations', 'OneHotEncoded_Map']
+    features = ['Simulations', 'Encoded_Map']
 
     if 'Encoded_Map' in features:
         features.remove('Encoded_Map')
@@ -111,16 +112,20 @@ if __name__ == "__main__":
     #'RandomForestRegressor': RandomForestRegressor(),
     #'GradientBoostingRegressor': GradientBoostingRegressor(),
     #'KNeighborsRegressor': KNeighborsRegressor(n_neighbors=5),
-    'MLPRegressor': MLPRegressor(hidden_layer_sizes=(50, 50), activation='tanh', learning_rate='adaptive', max_iter=100000)
+    'MLPRegressor': MLPRegressor(hidden_layer_sizes=(150, 150, 150, 150), activation='tanh', learning_rate='adaptive', max_iter=100000)
     }
 
-    train_sizes, train_scores, test_scores = learning_curve(models['MLPRegressor'], X[:size], y[:size], cv=5, train_sizes=np.append(np.linspace(0.01, 0.1, 10, endpoint=False), np.linspace(0.1, 1.0, 10)), scoring='r2', n_jobs=cores, verbose=2)
+    train_sizes, train_scores, test_scores = learning_curve(models['MLPRegressor'], X[:size], y[:size], cv=8, train_sizes=np.append(np.linspace(0.01, 0.1, 10, endpoint=False), np.linspace(0.1, 1.0, 10)), scoring='r2', n_jobs=cores, verbose=2)
 
     # Calculate the mean and standard deviation of the training and test scores
     train_mean = np.mean(train_scores, axis=1)
     train_std = np.std(train_scores, axis=1)
     test_mean = np.mean(test_scores, axis=1)
     test_std = np.std(test_scores, axis=1)
+    
+    models['MLPRegressor'].fit(X[:size], y[:size])
+    filename = file_dir('../results/after_bug/FrozenLake/FL_encodedmap_150-150-150-150.sav')
+    pickle.dump(models['MLPRegressor'], open(filename, 'wb'))
     
     # Plot the learning curve
     plt.figure(figsize=(10, 6))
@@ -135,8 +140,8 @@ if __name__ == "__main__":
     # Add labels and title
     plt.xlabel('Training Set Size')
     plt.ylabel('R2 Score')
-    plt.title('Learning Curve | CartPole-v1 | Temp=500 | MLPRegressor | Y=Discounted Return')
+    plt.title('Learning Curve | CartPole-v1 | Temp=1 | MLPRegressor (150,150,150,150)')
     plt.legend(loc='best')
-    plt.savefig(file_dir('../results/after_bug/CartPole/learning_curve_multifeature_maxiter.png'))
+    plt.savefig(file_dir('../results/after_bug/FrozenLake/learning_curve_maxiter.png'))
     df = pd.DataFrame({'train_sizes': train_sizes, 'train_mean': train_mean, 'train_std': train_std, 'test_mean': test_mean, 'test_std': test_std})
-    df.to_csv(file_dir('../results/after_bug/CartPole/learning_curve_multifeature_maxiter.csv'), index=False)
+    df.to_csv(file_dir('../results/after_bug/FrozenLake/learning_curve_maxiter.csv'), index=False)
