8df4676deb5a92a3fbe1ae73df69375fa8af86f3
Kerem <kerem.onr@gmail.com>
2023-08-05 23:10:04 +0300

    Fixed dataset creation

--- scripts/CreateDataset.py
+++ scripts/CreateDataset.py
@@ -17,12 +17,14 @@ if __name__ == "__main__":
     parser.add_argument('--environment', help='The environment argument, enter the name of the environment, eg. CartPole-v1')
     parser.add_argument('--dataset-name', help='The dataset_name argument, enter the name of the dataset, eg. 1-16_1000-Cartpole')
     parser.add_argument('--horizon', help='The horizon argument, enter the horizon, eg. 100')
+    parser.add_argument('--random-state')
 
     # Parse the arguments
     args = parser.parse_args()
     TEMPERATURE = int(args.temperature)
     SIMULATIONS = [int(simulation) for simulation in args.simulations.split(',')]
     SIZE = int(args.size)
+    initial_state_random = bool(args.random_state)
     if args.environment == 'FrozenLake-v1':
         env = StatelessGym.make("FrozenLake-v1", desc=None, map_name="4x4", is_slippery=False)
     else:
@@ -30,6 +32,6 @@ if __name__ == "__main__":
     
     agent = MCTS.mcts_agent(horizon=int(args.horizon))
     rand_experiment = RandomExperiment(env, agent, temperature=TEMPERATURE, simulations=SIMULATIONS)
-    rand_experiment.create_dataset(SIZE, args.dataset_name)
+    rand_experiment.create_dataset(SIZE, args.dataset_name, initial_state_random=initial_state_random)
     
     
